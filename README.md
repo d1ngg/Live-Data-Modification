370d3667d4b35eef45da9d2fe74e49fe1bf2a712
DINA GURUNG <gurungdin@deakin.edu.au>
2020-11-25 12:19:02 +1100

    added cytoscape model to web server

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Live-Data-Modification
 
 Install the following packages:
+npm
+npm install express
+npm install mongodb
 npm install cytoscape
---
+++ server.js
@@ -0,0 +1,43 @@
+// "start": "node server.js"
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const app = express();
+const https = require('https');
+var cytoscape = require('cytoscape');
+let edgehandles = require('cytoscape-edgehandles');
+
+cytoscape.use( edgehandles ); // register extension
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get("/auth/google/callback",function(req, res) {
+  console.log("Request is pringting");
+  console.log(req.query.name);
+  console.log(req.query.url);
+  https.get(req.query.url+"&user="+req.query.id, (response) => {
+    let data = "";
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      console.log("Before printing res json");
+      console.log(data);
+    });
+
+  })
+  .on('error', (error) => {
+    console.log(error);
+  });
+  
+  console.log("After printing after res json");
+  res.send();
+}
+)
+
+
+
+app.listen(port);
+console.log("Listening on port ", port);
